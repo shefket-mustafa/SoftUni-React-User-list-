2f7770e743270d8f9fd969c0a84c4cf557451fbc
shefket-mustafa <miracle_1@abv.bg>
2025-03-04 17:41:18 +0200

    Fetch users data saved in user state

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Pagination from "./Pagination";
 import Search from "./Search";
@@ -6,12 +6,13 @@ import UserListItem from "./UserListItem";
 import userService from "../services/userService";
 
 export default function UserList(){
+    const [users, setUsers] = useState([]);
 
 
     useEffect(()=>{
         userService.getAll()
         .then(result => {
-            console.log(Object.values(result)); 
+            setUsers(result);
         })
     }, []); 
 
